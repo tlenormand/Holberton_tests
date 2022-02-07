0c87ef70d68787483cf744f9a3653a627c544a35
jodt <joel.dumortier@gmail.com>
2022-02-07 14:48:18 +0000

    last version base test

--- holbertonschool-higher_level_programming/0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ holbertonschool-higher_level_programming/0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,150 +1,62 @@
 #!/usr/bin/python3
 """
-Unittest for "base.py"
-Execute all tests: python3 -m unittest discover tests
-Execute this test: python3 -m unittest tests/test_models/test_base.py
+This is the "test_base" module
+The test_base module suppies a class to test class Base
 """
 
 import unittest
-import pycodestyle
-from models import base
-Base = base.Base
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
     """
-    class that test the max integer function
-    Task 1:
-        you can assume id is an integer and you don’t need to test the
-            type of it
-    Tests:
-        test_many_created (working test):
-            no arguments when created Base
-            None arguments when created Base
-            Random integer argument when created Base
-            negative integer when creating Base
-            double same id when creating Base
-        test_too_many_arguments (no-working test):
-            too many arguments given
+    Test class Base
     """
 
-    def test_documentation(self):
-        """test all documentation of module"""
-        # module documentation
-        module = len(base.__doc__)
-        self.assertGreater(module, 0)
-
-        # class documentation
-        module_class = len(Base.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.__init__.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.to_json_string.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.save_to_file.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.from_json_string.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.create.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.load_from_file.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.save_to_file_csv.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.load_from_file_csv.__doc__)
-        self.assertGreater(module_class, 0)
-
-        module_class = len(Base.draw.__doc__)
-        self.assertGreater(module_class, 0)
-
-    def test_conformance(self):
-        """Test that we conform to PEP-8."""
-        style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(["models/base.py"])
-        self.assertEqual(
-            result.total_errors, 0, "Found code style errors (pycodestyle)."
-        )
-
-    def test_many_created(self):
-        """fuction that test for good assignment of differents id value"""
-        b1 = Base()
-        b2 = Base(None)
-        b3 = Base(12)
-        b4 = Base()
-        b5 = Base(-3)
-        b6 = Base(2)
-
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 12)
-        self.assertEqual(b4.id, 3)
-        self.assertEqual(b5.id, -3)
-        self.assertEqual(b6.id, 2)
-
-    def test_too_many_arguments(self):
+    def test_base_id(self):
         """
-        fuction that test for TypeError
+        This functions tests assigned id for class creation
         """
-        with self.assertRaises(TypeError):
-            Base(1, 2)
+        test_1 = Base()
+        test_2 = Base()
+        test_3 = Base(77)
+        test_4 = Base(None)
+        self.assertIsInstance(test_1, Base)
+        self.assertEqual(test_1.id, 1)
+        self.assertEqual(test_2.id, 2)
+        self.assertEqual(test_3.id, 77)
+        self.assertEqual(test_4.id, 3)
 
-    def test_toJsonString(self):
-        """Convert list to Json content"""
-        l = [{"Salut": 1, "Bonjour": 2}]
-        string = Base.to_json_string(l)
-        self.assertTrue(type(string), str)
-        self.assertEqual(string, '[{"Salut": 1, "Bonjour": 2}]')
+# ---------------------------------------------------------------
 
-    def test_toJsonString_empty(self):
-        """Convert empty list to Json content"""
-        l = []
-        string = Base.to_json_string(l)
-        self.assertTrue(type(string), str)
-        self.assertEqual(string, '[]')
+    def test_base_to_json_string(self):
+        """
+        This function test if the method to_json_string returns
+        the JSON string representation ofdictionaries list
+        """
+        dict_test = [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}]
+        self.assertTrue(type(Base.to_json_string(dict_test)), str)
+        self.assertEqual(Base.to_json_string(
+            dict_test), '[{"a": 1, "b": 2}, {"c": 3, "d": 4}]')
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string(None), "[]")
 
-    def test_toJsonString_None(self):
-        """Convert None to Json content"""
-        l = None
-        string = Base.to_json_string(l)
-        self.assertTrue(type(string), str)
-        self.assertEqual(string, '[]')
+# --------------------------------------------------------------
+    def test_base_from_json_string(self):
+        """
+        This function test if the method from_json_string returns
+        the list of the JSON string representation
+        """
+        json_str = '[{"a": 1, "b": 2}, {"c": 3, "d": 4}]'
+        self.assertTrue(type(Base.from_json_string(json_str)), list)
+        self.assertTrue(
+            all(type(elt) is dict for elt in Base.from_json_string(
+                json_str)), dict)
+        self.assertEqual(Base.from_json_string(json_str), [
+                         {"a": 1, "b": 2}, {"c": 3, "d": 4}])
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
 
-    def test_fromJsonString(self):
-        """Convert Json content to list"""
-        string = '[{"Salut": 1, "Bonjour": 2}]'
-        l = Base.from_json_string(string)
-        self.assertTrue(type(l), list)
-        self.assertEqual(l, [{"Salut": 1, "Bonjour": 2}])
-
-    def test_fromJsonStringList(self):
-        """Convert Json content to list"""
-        string = '[{"Salut": 1, "Bonjour": 2}, {"Hola": 1}]'
-        l = Base.from_json_string(string)
-        self.assertTrue(type(l), list)
-        self.assertEqual(l[0], {"Salut": 1, "Bonjour": 2})
-        self.assertEqual(l[1], {"Hola": 1})
-
-    def test_fromJsonString_None(self):
-        """Convert None Json content to list"""
-        string = None
-        l = Base.from_json_string(string)
-        self.assertTrue(type(l), list)
-        self.assertEqual(l, [])
-
-    def test_fromJsonString_len0(self):
-        """Convert empty Json content to list"""
-        string = ""
-        l = Base.from_json_string(string)
-        self.assertTrue(type(l), list)
-        self.assertEqual(l, [])
-
-if __name__ == "__main__":
-    unittest.main()
+#
